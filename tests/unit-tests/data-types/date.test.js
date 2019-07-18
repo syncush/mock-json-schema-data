3ddac95eb09934a95614a6604d92b71d14b69a96
syncush <danielhermonsync@gmail.com>
2019-07-18 20:20:42 +0300

    add - date tests , remove - unnecessary files

--- tests/unit-tests/data-types/date.test.js
+++ tests/unit-tests/data-types/date.test.js
@@ -10,4 +10,21 @@ const generateSchemaFromTemplate = (start, end, isUTC, format) => ({
   format
 });
 
-describe("unit-tests/data-types/date", () => {});
+const utcRegex = /\b[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z\b/;
+
+describe("unit-tests/data-types/date", () => {
+  it("isUTC = false", () => {
+    const dateString = mocker(generateSchemaFromTemplate(1990, 2000, false));
+    expect(utcRegex.test(dateString)).to.be.false;
+    expect(new Date(dateString).getFullYear())
+      .to.be.gte(1990)
+      .and.lte(2000);
+  });
+  it("isUTC = true", () => {
+    const dateString = mocker(generateSchemaFromTemplate(1990, 2000, true));
+    expect(utcRegex.test(dateString)).to.be.true;
+    expect(new Date(dateString).getUTCFullYear())
+      .to.be.gte(1990)
+      .and.lte(2000);
+  });
+});
